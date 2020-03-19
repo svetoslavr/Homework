5c8841649c26b5a41685ee20d21b766026018442
Daniel Vasilev <61119347+aANKFcrkfR@users.noreply.github.com>
2020-03-19 14:24:43 +0200

    Homework 03/19/2020

---
+++ Homework-03-19-2020/App3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] array = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            int odds = 0, evens = 0;
+
+            foreach (var number in array)
+            {
+                if (number % 2 == 0)
+                {
+                    evens += number;
+                }
+                else
+                {
+                    odds += number;
+                }
+            }
+
+            Console.WriteLine($"({evens}) - ({odds}) = {evens - odds}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Homework-03-19-2020/Homework/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string[] input = new string[text.Count()];
+            int f = 0;
+            foreach (var item in text)
+            {
+                input[f] = item.ToString();
+                f++;
+            }
+
+            string word = "";
+            for (int i = 3; i < text.Count(); i++)
+            {
+                word += input[i];
+            }
+
+            string word2 = "";
+            for (int i = 0; i < text.Count() - 3; i++)
+            {
+                word2 += input[i];
+            }
+
+            int n = int.Parse(Console.ReadLine());
+            string word3 = "";
+
+            for (int i = n; i < text.Count() - 3; i++)
+            {
+                word3 += input[i];
+            }
+
+            Console.WriteLine(word);
+            Console.WriteLine(word2);
+            Console.WriteLine(word3);
+            Console.Read();
+        }
+    }
+}
